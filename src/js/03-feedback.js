2f6b1d8c9424499ac7bef035cdf06b725e1e92c6
Petr986 <93924783+Petr986@users.noreply.github.com>
2022-06-28 13:25:14 +0300

    finale-commit

---
+++ src/js/03-feedback.js
@@ -0,0 +1,46 @@
+import throttle from 'lodash.throttle';
+
+//======================================\\
+//const
+const STORAGE_KEY = 'feedback-form-state';
+
+//refs
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('[name="email"]');
+const textArea = document.querySelector('[name="message"]');
+const btn = document.querySelector('[type="submit"]');
+
+//liseners
+form.addEventListener('submit', onSubmit);
+form.addEventListener('input', throttle(onInputForm, 500));
+
+pushToInputs();
+
+const dataUser = { [input.name]: input.value, [textArea.name]: textArea.value };
+localStorage.setItem(STORAGE_KEY, JSON.stringify(dataUser));
+
+function onSubmit(event) {
+  event.preventDefault();
+  if (input.value === '' || textArea.value === '') {
+    return alert('Будь ласка, заповніть всі поля!');
+  }
+  console.log(dataUser);
+  event.target.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onInputForm(evt) {
+  dataUser[input.name] = input.value;
+  dataUser[textArea.name] = textArea.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(dataUser));
+}
+
+function pushToInputs() {
+  const savedMsg = localStorage.getItem(STORAGE_KEY);
+  const currentDatsUser = JSON.parse(savedMsg);
+  if (!currentDatsUser) {
+    return;
+  }
+  input.value = currentDatsUser.email;
+  textArea.value = currentDatsUser.message;
+}
